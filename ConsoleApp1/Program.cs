c87616de27cea6b947e0bae6c654f392a4bf0b9f
modi_ami <modi_ami@outlook.com>
2017-12-09 10:10:37 -0800

    Third Changes

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hi this is first change");
+            Console.WriteLine("This is second change");
         }
     }
 }
